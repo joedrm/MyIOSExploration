7e11d3db9b71617c2fbc598f020d5364e71f8d43
冬日暖阳 <wdy@wdydeMacBook-Pro.local>
2016-10-20 20:13:54 +0800

    新增

---
+++ MyIOSExploration/NavigationBarTransition转场动画/NavigationBarTransition转场动画/TransitionTestVC.m
@@ -0,0 +1,23 @@
+//
+//  TransitionTestVC.m
+//  转场动画
+//
+//  Created by wdy on 2016/10/20.
+//  Copyright © 2016年 com.wdy.MyIOSExploration. All rights reserved.
+//
+
+#import "TransitionTestVC.h"
+
+@interface TransitionTestVC ()
+
+@end
+
+@implementation TransitionTestVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+
+@end
---
+++ MyIOSExploration/XML解析/XML解析/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  XML解析
+//
+//  Created by wdy on 2016/10/20.
+//  Copyright © 2016年 com.wdy.MyIOSExploration. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //http://lolbox.duowan.com/playerDetail.php?serverName=%E8%89%BE%E6%AC%A7%E5%B0%BC%E4%BA%9A&playerName=%E4%BD%A0%E6%83%B3%E7%9C%8B%E7%A9%BF%E4%BB%80%E4%B9%88
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
