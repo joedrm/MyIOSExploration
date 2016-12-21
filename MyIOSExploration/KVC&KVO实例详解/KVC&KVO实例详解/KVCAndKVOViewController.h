4797de01ddc084868cd0098213a8a862a2421cb8
冬日暖阳 <877197753@qq.com>
2016-12-21 16:23:14 +0800

    KVC&KVO

---
+++ MyIOSExploration/KVC&KVO实例详解/KVC&KVO实例详解/KVCAndKVOViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVC&KVO实例详解
+//
+//  Created by fang wang on 16/12/21.
+//  Copyright © 2016年 wdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KVCAndKVOViewController : UITableViewController
+
+
+@end
+
---
+++ MyIOSExploration/KVC&KVO实例详解/KVC&KVO实例详解/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KVC&KVO实例详解
+//
+//  Created by fang wang on 16/12/21.
+//  Copyright © 2016年 wdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
