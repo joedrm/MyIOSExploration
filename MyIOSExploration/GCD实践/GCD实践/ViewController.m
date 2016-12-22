92ae4073448ad54576807e8fac3861afaec6ed24
冬日暖阳 <877197753@qq.com>
2016-12-22 14:42:33 +0800

    small change

--- MyIOSExploration/GCD实践/GCD实践/ViewController.m
+++ MyIOSExploration/GCD实践/GCD实践/ViewController.m
@@ -861,22 +861,20 @@ static const void * const kDispatchQueueSpecificKey = &kDispatchQueueSpecificKey
  并发队列：用来执行与UI无关的后台任务，dispatch_sync放在这里，方便等待任务完成进行后续处理或和dispatch barrier同步。dispatch groups放在这里也不错。
  自定义顺序队列：顺序执行后台任务并追踪它时。这样做同时只有一个任务在执行可以防止资源竞争。dipatch barriers解决读写锁问题的放在这里处理。dispatch groups也是放在这里。
  
- 参考资料：
- 
- iOS多线程之GCD：http://www.jianshu.com/p/456672967e75
- iOS笔记(一)GCD多线程：信号量和条件锁: https://my.oschina.net/u/2436242/blog/518318
- 细说GCD（Grand Central Dispatch）如何用: http://www.jianshu.com/p/fbe6a654604c
- */
-
-/*
  GCD和NSOperationQueue的区别：
- 
  1.GCD是底层的C语言构成的API，而NSOperationQueue及相关对象是Objc的对象。在GCD中，在队列中执行的是由block构成的任务，这是一个轻量级的数据结构；而Operation作为一个对象，为我们提供了更多的选择；
  2.在NSOperationQueue中，我们可以随时取消已经设定要准备执行的任务(当然，已经开始的任务就无法阻止了)，而GCD没法停止已经加入queue的block(其实是有的，但需要许多复杂的代码)；
  3.NSOperation能够方便地设置依赖关系，我们可以让一个Operation依赖于另一个Operation，这样的话尽管两个Operation处于同一个并行队列中，但前者会直到后者执行完毕后再执行；
  4.我们能将KVO应用在NSOperation中，可以监听一个Operation是否完成或取消，这样子能比GCD更加有效地掌控我们执行的后台任务；
  5.在NSOperation中，我们能够设置NSOperation的priority优先级，能够使同一个并行队列中的任务区分先后地执行，而在GCD中，我们只能区分不同任务队列的优先级，如果要区分block任务的优先级，也需要大量的复杂代码；
  6.我们能够对NSOperation进行继承，在这之上添加成员变量与成员方法，提高整个代码的复用度，这比简单地将block任务排入执行队列更有自由度，能够在其之上添加更多自定制的功能。
+ 
+ 
+ 参考资料：
+ 
+ iOS多线程之GCD：http://www.jianshu.com/p/456672967e75
+ iOS笔记(一)GCD多线程：信号量和条件锁: https://my.oschina.net/u/2436242/blog/518318
+ 细说GCD（Grand Central Dispatch）如何用: http://www.jianshu.com/p/fbe6a654604c
 
 */
 
