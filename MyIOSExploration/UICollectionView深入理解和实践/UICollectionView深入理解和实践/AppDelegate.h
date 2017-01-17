f2e9519065c9684387459fb50c153e971a5194e0
冬日暖阳 <877197753@qq.com>
2017-01-17 10:07:34 +0800

    添加UICollectionView pre

---
+++ MyIOSExploration/UICollectionView深入理解和实践/UICollectionView深入理解和实践/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionView深入理解和实践
+//
+//  Created by fang wang on 17/1/17.
+//  Copyright © 2017年 wdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyIOSExploration/UICollectionView深入理解和实践/UICollectionView深入理解和实践/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  UICollectionView深入理解和实践
+//
+//  Created by fang wang on 17/1/17.
+//  Copyright © 2017年 wdy. All rights reserved.
+//
+/*
+ 参考资料：
+ https://github.com/Instagram/IGListKit  近期关注度比较高的UICollectionView框架
+ */
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
