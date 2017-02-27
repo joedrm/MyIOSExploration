fd0d1d1f952dabf256c1177411bc7c71432d7649
wangfang <fang.wang@lebaoedu.com>
2017-02-27 19:16:33 +0800

    change
    Signed-off-by: wangfang <fang.wang@lebaoedu.com>

---
+++ MyIOSExploration/UICollectionView深入理解和实践/UICollectionView深入理解和实践/CollectionMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  CollectionMainViewController.h
+//  UICollectionView深入理解和实践
+//
+//  Created by fang wang on 17/2/27.
+//  Copyright © 2017年 wdy. All rights reserved.
+//
+
+#import <WDYLibrary/WDYLibrary.h>
+
+@interface CollectionMainViewController : BaseTestDataTableVC
+
+@end
---
+++ MyIOSExploration/UICollectionView深入理解和实践/UICollectionView深入理解和实践/CollectionMainViewController.m
@@ -0,0 +1,44 @@
+//
+//  CollectionMainViewController.m
+//  UICollectionView深入理解和实践
+//
+//  Created by fang wang on 17/2/27.
+//  Copyright © 2017年 wdy. All rights reserved.
+//
+
+#import "CollectionMainViewController.h"
+
+@interface CollectionMainViewController ()
+
+@end
+
+@implementation CollectionMainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    self.title = @"UICollectionView深入理解和实践";
+    self.titleArr = @[
+                      ];
+    
+    self.vcArr = @[
+                   
+                   ];
+}
+
+
+
+@end
+
+
+/*
+ 参考资料：
+ https://github.com/Instagram/IGListKit  近期关注度比较高的UICollectionView框架
+ 
+ http://www.jianshu.com/p/d75a9a8d13b5
+ 
+ https://github.com/KelvinJin/AnimatedCollectionViewLayout   CollectionView切换动画
+ 
+ https://github.com/Tidusww/WWCollectionWaterfallLayout  UICollectionView 实现瀑布流布局
+ */
